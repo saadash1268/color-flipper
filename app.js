7a2c7c62bb1d5dc6732b823c55ffd17aec5cd6aa
Saad Ashraf <saadash1268@gmail.com>
2023-11-28 16:08:35 +0000

    final changes

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener('click', function(){
-    //get random number between 0 - 3
     const randomNumber = getRandomNumber();
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber]
